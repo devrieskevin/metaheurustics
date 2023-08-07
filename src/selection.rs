20c99032d6131e5c8866b6ed4ae6b5338a7dd662
Kevin de Vries <kevin@aubergine-it.nl>
2023-08-07 23:53:20 +0200

    Reimplement `stochastic_universal_sampling`
    - Also refactor functions to parent selection sub module

--- src/selection.rs
+++ src/selection.rs
@@ -1,26 +1,2 @@
-use ndarray_rand::{rand::{self, Rng, thread_rng}, rand_distr::{Uniform, WeightedIndex}};
-
-
-use crate::population::Population;
-
-mod survivor;
 mod parent;
-
-/// @todo Use stochastic universal sampling instead of roulette wheel selection
-pub fn select_fitness_proportionate(population: &Population<f64>) -> Population<f64> {
-    let mut population = population.clone();
-    population.individuals.sort_by(|a, b| b.compare_fitness(a));
-
-    let dist = WeightedIndex::new(
-        population.individuals
-            .iter()
-            .map(|individual| individual.fitness)
-    ).unwrap();
-
-    population.individuals = thread_rng().sample_iter(dist)
-        .take(population.individuals.len())
-        .map(|i| population.individuals[i].clone())
-        .collect();
-
-    population
-}
+mod survivor;
