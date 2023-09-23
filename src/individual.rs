01369d200254a5ac00c3155c29ef18ce8e06da5f
Kevin de Vries <kevin@aubergine-it.nl>
2023-09-24 00:32:40 +0200

    Issue #57 Implement recombinator + mutator
    - For `BoundedVectorIndividual`

--- src/individual.rs
+++ src/individual.rs
@@ -1,6 +1,6 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, marker::PhantomData};
 
-use crate::parameter::BoundedVector;
+use crate::{mutation::Mutator, parameter::BoundedVector, recombination::Recombinator};
 
 pub trait Individual<F>
 where
@@ -132,6 +132,108 @@ where
 {
     pub fn vector(&self) -> &BoundedVector<T> {
         &self.vector
+    }
+}
+
+impl<T, F> BoundedVectorIndividual<T, F>
+where
+    T: PartialOrd,
+    F: PartialOrd + Default,
+{
+    pub fn new(vector: BoundedVector<T>) -> Self {
+        Self {
+            vector,
+            fitness: Default::default(),
+            age: 0,
+        }
+    }
+}
+
+pub struct BoundedVectorIndividualMutator<T, M>
+where
+    T: PartialOrd,
+    M: Mutator<BoundedVector<T>>,
+{
+    vector_mutator: M,
+    _phantom: PhantomData<T>,
+}
+
+impl<T, M> BoundedVectorIndividualMutator<T, M>
+where
+    T: PartialOrd,
+    M: Mutator<BoundedVector<T>>,
+{
+    pub fn new(vector_mutator: M) -> Self {
+        Self {
+            vector_mutator,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<T, M, F> Mutator<BoundedVectorIndividual<T, F>> for BoundedVectorIndividualMutator<T, M>
+where
+    T: PartialOrd,
+    M: Mutator<BoundedVector<T>>,
+    F: PartialOrd + Copy + Default,
+{
+    fn mutate<'a, R: rand::Rng + ?Sized>(
+        &self,
+        rng: &mut R,
+        parameter: &'a mut BoundedVectorIndividual<T, F>,
+    ) -> &'a mut BoundedVectorIndividual<T, F> {
+        self.vector_mutator.mutate(rng, &mut parameter.vector);
+        parameter
+    }
+}
+
+pub struct BoundedVectorIndividualRecombinator<T, R, const N: usize>
+where
+    T: PartialOrd,
+    R: Recombinator<BoundedVector<T>, N>,
+{
+    vector_recombinator: R,
+    _phantom: PhantomData<T>,
+}
+
+impl<T, R, const N: usize> BoundedVectorIndividualRecombinator<T, R, N>
+where
+    T: PartialOrd,
+    R: Recombinator<BoundedVector<T>, N>,
+{
+    pub fn new(vector_recombinator: R) -> Self {
+        Self {
+            vector_recombinator,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<C, T, F, const N: usize> Recombinator<BoundedVectorIndividual<T, F>, N>
+    for BoundedVectorIndividualRecombinator<T, C, N>
+where
+    T: PartialOrd,
+    C: Recombinator<BoundedVector<T>, N>,
+    F: PartialOrd + Copy + Default,
+{
+    fn recombine<R: rand::Rng + ?Sized>(
+        &self,
+        rng: &mut R,
+        parents: &[&BoundedVectorIndividual<T, F>; N],
+    ) -> [BoundedVectorIndividual<T, F>; N] {
+        let values: Vec<_> = parents.iter().map(|parent| parent.vector()).collect();
+        let values_array = values[..].try_into().unwrap();
+        let recombined_individuals: Vec<_> = self
+            .vector_recombinator
+            .recombine(rng, values_array)
+            .into_iter()
+            .map(|value| BoundedVectorIndividual::new(value))
+            .collect();
+
+        match recombined_individuals.try_into() {
+            Ok(x) => x,
+            Err(_) => panic!("Recombination resulted in an unexpected amount of offspring."),
+        }
     }
 }
 
--- src/parameter.rs
+++ src/parameter.rs
@@ -1,77 +1,4 @@
-use std::rc::Rc;
-
-use rand::Rng;
-
-use crate::{mutation::Mutator, recombination::Recombinator};
-
-pub trait Parameter<const N: usize> {
-    fn mutate<R: Rng + ?Sized>(&mut self, rng: &mut R) -> &mut Self;
-
-    fn recombine<R: Rng + ?Sized>(&self, rng: &mut R, parents: &[&Self; N]) -> [Self; N]
-    where
-        Self: Sized;
-}
-
-pub struct ParameterWrapper<T, R, const N: usize, M>
-where
-    R: Recombinator<T, N>,
-    M: Mutator<T>,
-{
-    value: T,
-    recombinator: Rc<R>,
-    mutator: Rc<M>,
-}
-
-impl<T, C, const N: usize, M> ParameterWrapper<T, C, N, M>
-where
-    C: Recombinator<T, N>,
-    M: Mutator<T>,
-{
-    pub fn new(value: T, recombinator: Rc<C>, mutator: Rc<M>) -> Self {
-        Self {
-            value,
-            recombinator,
-            mutator,
-        }
-    }
-}
-
-impl<T, C, const N: usize, M> Parameter<N> for ParameterWrapper<T, C, N, M>
-where
-    C: Recombinator<T, N>,
-    M: Mutator<T>,
-{
-    fn mutate<R: Rng + ?Sized>(&mut self, rng: &mut R) -> &mut Self {
-        self.mutator.mutate(rng, &mut self.value);
-        self
-    }
-
-    fn recombine<R: Rng + ?Sized>(&self, rng: &mut R, parents: &[&Self; N]) -> [Self; N]
-    where
-        Self: Sized,
-    {
-        let values: Vec<_> = parents.iter().map(|parent| &parent.value).collect();
-        let values_array: &[&T; N] = values[..].try_into().unwrap();
-
-        let recombined: Vec<_> = self
-            .recombinator
-            .recombine(rng, values_array)
-            .into_iter()
-            .map(|value| {
-                Self::new(
-                    value,
-                    Rc::clone(&self.recombinator),
-                    Rc::clone(&self.mutator),
-                )
-            })
-            .collect();
-
-        match recombined.try_into() {
-            Ok(x) => x,
-            Err(_) => panic!("Recombination resulted in an unexpected amount of offspring."),
-        }
-    }
-}
+pub trait Parameter {}
 
 #[derive(Clone)]
 pub struct BoundedVector<T>
@@ -82,3 +9,5 @@ where
     pub max_value: T,
     pub value: Vec<T>,
 }
+
+impl<T> Parameter for BoundedVector<T> where T: PartialOrd {}
