9924f2181bbdda1fc1c1436f803f891cc2b097ee
Kevin de Vries <kevin@aubergine-it.nl>
2023-11-06 01:57:14 +0100

    #75 Implement n-point crossover

--- src/recombination/integer.rs
+++ src/recombination/integer.rs
@@ -1,4 +1,6 @@
-use rand::Rng;
+use std::num::NonZeroUsize;
+
+use rand::{seq::IteratorRandom, Rng};
 
 use super::Recombinator;
 
@@ -13,6 +15,40 @@ impl Recombinator<i32, 2> for OnePoint {
 
         let child_1 = (parent_1 & (ALL_BITS << mask)) | (parent_2 & !(ALL_BITS << mask));
         let child_2 = (parent_2 & (ALL_BITS << mask)) | (parent_1 & !(ALL_BITS << mask));
+
+        [child_1, child_2]
+    }
+}
+
+pub struct NPoint {
+    points: NonZeroUsize,
+}
+
+impl NPoint {
+    pub fn new(points: NonZeroUsize) -> Self {
+        Self { points }
+    }
+}
+
+impl Recombinator<i32, 2> for NPoint {
+    fn recombine<R: Rng + ?Sized>(&self, rng: &mut R, parents: &[&i32; 2]) -> [i32; 2] {
+        const ALL_BITS: i32 = !0;
+
+        let [&parent_1, &parent_2] = parents;
+
+        let mut points = (1..i32::BITS).choose_multiple(rng, self.points.get());
+        points.push(0);
+        points.push(i32::BITS);
+        points.sort_unstable();
+
+        let mask = points.windows(2).step_by(2).fold(0, |acc, slice| {
+            let [left_bound, right_bound]: [_; 2] = slice.try_into().unwrap();
+            let one_bits = right_bound - left_bound;
+            acc | (!(ALL_BITS << one_bits) << left_bound)
+        });
+
+        let child_1 = (parent_1 & mask) | (parent_2 & !mask);
+        let child_2 = (parent_2 & mask) | (parent_1 & !mask);
 
         [child_1, child_2]
     }
